7b504bde462194b4efd5492d157bcb75b2966753
Abdul Mueed <abdulmueed1992@gmail.com>
2023-02-06 14:38:16 +1100

    commit

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 from services.twitter import TwitterService
-import tweepy
-
 
 if __name__ == "__main__":
-    # # Get the top 20 tweets for each trend
-    # tweets = []
-    # for trend in usa_trends[0]["trends"]:
-    #     tweets.extend(api.search_tweets(q=trend["name"], count=20))
-    # for trend in aus_trends[0]["trends"]:
-    #     tweets.extend(api.search_tweets(q=trend["name"], count=20))
-
     # twitterService = TwitterService()    
     # twitterService.connectToTwitter()
     # trends = twitterService.getAllTrends()
